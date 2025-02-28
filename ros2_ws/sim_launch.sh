916c274aea58e5f57087bc675d87db1448ee23bf
Nelson Durrant <snelsondurrant@gmail.com>
2025-02-28 02:54:13 -0700

    Renamed launch file

--- ros2_ws/sim_launch.sh
+++ ros2_ws/sim_launch.sh
@@ -2,4 +2,4 @@
 # Created by Nelson Durrant, Feb 2025
 
 source ~/ros2_ws/install/setup.bash
-ros2 launch nav2_autonomy state_machine_sim.launch.py use_rviz:=True use_mapviz:=True
+ros2 launch nav2_autonomy nav2_autonomy_sim.launch.py use_rviz:=True use_mapviz:=True
