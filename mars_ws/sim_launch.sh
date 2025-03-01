a6cc0bc62c1a2d3af2d451676aaa89f7ee69f698
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-01 04:09:50 -0700

    autolauch mapviz container

--- mars_ws/sim_launch.sh
+++ mars_ws/sim_launch.sh
@@ -3,5 +3,7 @@
 #
 # Simply launches the full simulation stack
 
+sudo docker run -p 8080:8080 -d -t -v ~/mapproxy:/mapproxy danielsnider/mapproxy
+
 source ~/mars_ws/install/setup.bash
 ros2 launch nav2_autonomy full_stack_sim.launch.py use_rviz:=True use_mapviz:=True
