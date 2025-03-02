103c70f893b00fdeb81738a185d91860b123168d
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-02 03:32:22 -0700

    Quick fix

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -16,8 +16,6 @@ echo "export ROS_DISCOVERY_SERVER=127.0.0.1:11811" >> ~/.bashrc
 # Are we running on Jetson Orin architecture (the rover)?
 if [ "$(uname -m)" == "aarch64" ]; then
 
-    fastdds discovery --server-id 0 & # start on port 11811
-
     # Start a new 'rover_runtime' tmux session
     tmux new-session -d -s rover_runtime
     tmux send-keys -t rover_runtime.0 "clear" Enter
@@ -25,7 +23,8 @@ if [ "$(uname -m)" == "aarch64" ]; then
     # Launch ROS 2 nodes on system startup
     tmux send-keys -t rover_runtime.0 "source ~/mars_ws/install/setup.bash" Enter
     tmux send-keys -t rover_runtime.0 "ros2 launch mobility mobility.launch.py" Enter
-    tmux send-keys -t rover_runtime.0 "ros2 launch peripherals peripherals.launch.py" Enter
+    # TODO: Combine these into a single launch file so we can fit them in one pane
+    # tmux send-keys -t rover_runtime.0 "ros2 launch peripherals peripherals.launch.py" Enter
 
     # Full color and mouse options
     tmux set-option -g default-terminal "screen-256color"
