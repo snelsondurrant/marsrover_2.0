dec7455f2789e41165a0d94d372411cd45431a95
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-03 14:01:25 -0700

    testing

--- mars_ws/src/nav2_autonomy/nav2_autonomy/state_machine.py
+++ mars_ws/src/nav2_autonomy/nav2_autonomy/state_machine.py
@@ -13,9 +13,10 @@ import time
 import threading
 
 from nav2_autonomy.utils.gps_utils import latLonYaw2Geopose
+from nav2_autonomy.utils.plan_utils import basicPathPlanner
 
 
-class YamlArucoWaypointParser:
+class YamlParser:
     """
     Parse a set of legs, aruco tags, and GPS waypoints from a yaml file
     """
@@ -26,32 +27,31 @@ class YamlArucoWaypointParser:
 
     def get_legs(self):
         """
-        Get an array of leg names from the yaml file
+        Get an array of leg strings from the yaml file
         """
 
         legs = []
         for leg in self.wps_dict["legs"]:
             legs.append(leg["leg"])
         return legs
-
-    def get_wps(self, leg):
+    
+    def get_tags(self):
         """
-        Get an array of ids and geographic_msgs/msg/GeoPose objects from the yaml file
+        Get an array of aruco tag yaml objects from the yaml file
         """
         aruco_tags = []
         for tag in self.wps_dict["aruco_tags"]:
-            if tag["leg"] == leg:
-                aruco_tags.append(tag)
-        gepose_wps = []
+            aruco_tags.append(tag)
+        return aruco_tags
+
+    def get_wps(self):
+        """
+        Get an array of GPS waypoint yaml objects from the yaml file
+        """
+        gps_wps = []
         for wp in self.wps_dict["waypoints"]:
-            if wp["leg"] == leg:
-                latitude, longitude, yaw = (
-                    wp["latitude"],
-                    wp["longitude"],
-                    wp["yaw"],
-                )  # Need to generate intermittent values somehow
-                gepose_wps.append(latLonYaw2Geopose(latitude, longitude, yaw))
-        return aruco_tags, gepose_wps
+            gps_wps.append(wp)
+        return gps_wps
 
 
 class StateMachine(Node):
@@ -62,12 +62,12 @@ class StateMachine(Node):
     :date: Feb 2025
 
     Subscribers:
+    - gps/filtered (sensor_msgs/NavSatFix)
     - aruco_detections (aruco_opencv_msgs/ArucoDetection)
     - zed/detections (vision_msgs/Detection3DArray)
     Publishers:
     - mapviz/goal (sensor_msgs/NavSatFix)
     - mapviz/inter (sensor_msgs/NavSatFix)
-    - mapviz/hex (sensor_msgs/NavSatFix)
     Clients:
     - trigger_teleop (std_srvs/Trigger)
     - trigger_auto (std_srvs/Trigger)
@@ -81,24 +81,32 @@ class StateMachine(Node):
         super().__init__("nav2_state_machine")
         self.navigator = BasicNavigator()
 
+        # Parse the waypoint file
         self.declare_parameter("wps_file_path", "")
         if not self.get_parameter("wps_file_path").value:
             self.get_logger().fatal("No waypoint file path provided")
             rclpy.shutdown()
 
         wps_file_path = self.get_parameter("wps_file_path").value
-        self.wp_parser = YamlArucoWaypointParser(wps_file_path)
+        self.wp_parser = YamlParser(wps_file_path)
 
-        self.legs = self.wp_parser.get_legs()
-        self.gps_legs = ["start", "gps1", "gps2"]
+        self.legs = self.wp_parser.get_legs() # array of strings
+        self.tags = self.wp_parser.get_tags() # array of yaml
+        self.wps = self.wp_parser.get_wps() # array of yaml
+
+        # Define the acceptable legs
+        self.gps_legs = ["gps1", "gps2"]
         self.aruco_legs = ["aruco1", "aruco2", "aruco3"]
         self.obj_legs = ["mallet", "bottle"]
 
+        # Initialize variables
+        self.leg = None
+        self.filtered_gps = None
         self.run_flag = False
         self.found_flag = False
-        self.tags = []
-        self.wps = []
+        self.found_pose = None
 
+        # Hex pattern for searching
         self.hex_coord = [
             (2.0, 0.0),
             (1.0, 1.73),
@@ -107,7 +115,18 @@ class StateMachine(Node):
             (-1.0, -1.73),
             (1.0, -1.73),
         ]
-        self.hex_scalar = 0.00001
+        self.hex_scalar = 0.00005
+
+        # GPS location (filtered) subscriber
+        gps_callback_group = MutuallyExclusiveCallbackGroup()
+        self.gps_subscriber = self.create_subscription(
+            NavSatFix,
+            "gps/filtered",
+            self.gps_callback,
+            10,
+            callback_group=gps_callback_group,
+        )
+        self.gps_subscriber  # prevent unused variable warning
 
         # Aruco pose subscriber
         aruco_callback_group = MutuallyExclusiveCallbackGroup()
@@ -127,7 +146,6 @@ class StateMachine(Node):
         self.mapviz_inter_publisher = self.create_publisher(
             NavSatFix, "mapviz/inter", 10
         )
-        self.mapviz_hex_publisher = self.create_publisher(NavSatFix, "mapviz/hex", 10)
 
         trigger_callback_group = MutuallyExclusiveCallbackGroup()
 
@@ -193,6 +211,13 @@ class StateMachine(Node):
         )
 
         return response
+    
+    def gps_callback(self, msg):
+        """
+        Callback function for the GPS subscriber
+        """
+
+        self.filtered_gps = latLonYaw2Geopose(msg.latitude, msg.longitude)
 
     def aruco_callback(self, msg):
         """
@@ -213,69 +238,30 @@ class StateMachine(Node):
                     "Found aruco tag: " + marker.marker_id, throttle_duration_sec=1
                 )
                 self.found_flag = True
-                self.found_pose = marker.pose
+                self.found_pose = marker.pose # TODO: Convert to GeoPose
 
-    def pose_nav(self, leg, pose):
-        """
-        Function to navigate to a pose
-        """
-
-        self.get_logger().info(leg + " Starting pose navigation")
-
-        self.navigator.goToPose(pose)
-        while not self.navigator.isTaskComplete():
-            time.sleep(0.1)
-
-        result = self.navigator.getResult()
-        if result == TaskResult.SUCCEEDED:
-            self.get_logger().info(leg + " Pose navigation completed")
-        elif result == TaskResult.CANCELED:
-            self.get_logger().warn(leg + " Pose navigation canceled")
-        elif result == TaskResult.FAILED:
-            self.get_logger().error(leg + " Pose navigation failed")
-
-    def gps_point_nav(self, leg, wp):
-        """
-        Function to navigate to a GPS point
-        """
-
-        self.get_logger().info(leg + " Starting GPS point navigation")
-
-        # Publish the GPS position to mapviz
-        navsat_fix = NavSatFix()
-        navsat_fix.header.frame_id = "map"
-        navsat_fix.header.stamp = self.navigator.get_clock().now().to_msg()
-        navsat_fix.latitude = wp.position.latitude
-        navsat_fix.longitude = wp.position.longitude
-        self.mapviz_hex_publisher.publish(navsat_fix)
-
-        single_wp = [wp] # Fix for the navigator
-
-        self.navigator.followGpsWaypoints(single_wp)
-        while not self.navigator.isTaskComplete():
-            time.sleep(0.1)
-
-        result = self.navigator.getResult()
-        if result == TaskResult.SUCCEEDED:
-            self.get_logger().info(leg + " GPS point navigation completed")
-        elif result == TaskResult.CANCELED:
-            self.get_logger().warn(leg + " GPS point navigation canceled")
-        elif result == TaskResult.FAILED:
-            self.get_logger().error(leg + " GPS point navigation failed")
-
-    def gps_path_nav(self, leg):
+    def gps_nav(self, dest_wp=None):
         """
         Function to navigate through GPS waypoints
         """
 
-        self.get_logger().info(leg + " Starting GPS path navigation")
+        self.get_logger().info(self.leg + " Starting GPS path navigation")
 
+        # If no waypoint is provided, navigate to the current leg's GPS waypoint
+        if not dest_wp:
+            for wp in self.wps:
+                if wp["leg"] == self.leg:
+                    dest_wp = latLonYaw2Geopose(wp["latitude"], wp["longitude"])
         
-        # Store relevant tags and waypoints
-        self.tags, self.wps = self.wp_parser.get_wps(leg)
+        if not dest_wp:
+            self.get_logger().fatal(self.leg + " No GPS waypoint defined for leg")
+            return
+        
+        # Generate a path from the current GPS location to the end GPS location
+        path = basicPathPlanner(self.filtered_gps, dest_wp)
 
-        for wp in self.wps:
-            # Publish the GPS positions to mapviz
+        # Publish the GPS positions to mapviz
+        for wp in path:
             navsat_fix = NavSatFix()
             navsat_fix.header.frame_id = "map"
             navsat_fix.header.stamp = self.navigator.get_clock().now().to_msg()
@@ -283,43 +269,43 @@ class StateMachine(Node):
             navsat_fix.longitude = wp.position.longitude
 
             # Publish to different topics based on type
-            if wp != self.wps[-1]:
+            if wp != dest_wp:
                 self.mapviz_inter_publisher.publish(navsat_fix)
             else:
                 self.mapviz_goal_publisher.publish(navsat_fix)
 
-        self.navigator.followGpsWaypoints(self.wps)
+        self.navigator.followGpsWaypoints(path)
         while not self.navigator.isTaskComplete():
             time.sleep(0.1)
 
         result = self.navigator.getResult()
         if result == TaskResult.SUCCEEDED:
-            self.get_logger().info(leg + " GPS path navigation completed")
+            self.get_logger().info(self.leg + " GPS navigation completed")
         elif result == TaskResult.CANCELED:
-            self.get_logger().warn(leg + " GPS path navigation canceled")
+            self.get_logger().warn(self.leg + " GPS navigation canceled")
         elif result == TaskResult.FAILED:
-            self.get_logger().error(leg + " GPS path navigation failed")
+            self.get_logger().error(self.leg + " GPS navigation failed")
 
-    def spin_search(self, leg):
+    def spin_search(self):
         """
         Function to spin in place
         """
 
-        self.get_logger().info(leg + " Starting spin search")
+        self.get_logger().info(self.leg + " Starting spin search")
 
         self.navigator.spin(spin_dist=3.14)
         while not self.navigator.isTaskComplete():
 
-            if leg in self.aruco_legs:
+            if self.leg in self.aruco_legs:
                 # Check for the aruco tag
-                pose = self.aruco_check(leg)
+                pose = self.aruco_check()
                 if pose:
                     self.navigator.cancelTask()
                     return pose
 
-            elif leg in self.obj_legs:
+            elif self.leg in self.obj_legs:
                 # Check for the object
-                pose = self.obj_check(leg)
+                pose = self.obj_check()
                 if pose:
                     self.navigator.cancelTask()
                     return pose
@@ -328,48 +314,60 @@ class StateMachine(Node):
 
         result = self.navigator.getResult()
         if result == TaskResult.SUCCEEDED:
-            self.get_logger().info(leg + " Spin search completed")
+            self.get_logger().info(self.leg + " Spin search completed")
         elif result == TaskResult.CANCELED:
-            self.get_logger().warn(leg + " Spin search canceled")
+            self.get_logger().warn(self.leg + " Spin search canceled")
         elif result == TaskResult.FAILED:
-            self.get_logger().error(leg + " Spin search failed")
+            self.get_logger().error(self.leg + " Spin search failed")
 
         return False
 
-    def hex_search(self, leg):
+    def hex_search(self):
         """
         Function to search in a hex pattern
         """
 
-        self.get_logger().info(leg + " Starting hex search")
+        self.get_logger().info(self.leg + " Starting hex search")
+
+        # Get this leg's GPS waypoint for math
+        base_wp = None
+        for wp in self.wps:
+            if wp["leg"] == self.leg:
+                base_wp = latLonYaw2Geopose(wp["latitude"], wp["longitude"])
+
+        if not base_wp:
+            self.get_logger().fatal(self.leg + " No GPS waypoint defined for leg")
+            return False
 
         # Generate a hex pattern in the base_link frame
         for coord in self.hex_coord:
             # Convert the hex pattern to GPS points using our last goal
-            hex_lat = self.wps[-1].position.latitude + coord[0] * self.hex_scalar
-            hex_lon = self.wps[-1].position.longitude + coord[1] * self.hex_scalar
-            hex_pose = latLonYaw2Geopose(hex_lat, hex_lon)
+            hex_lat = base_wp.position.latitude + coord[0] * self.hex_scalar
+            hex_lon = base_wp.position.longitude + coord[1] * self.hex_scalar
+            hex_wp = latLonYaw2Geopose(hex_lat, hex_lon)
 
-            self.gps_point_nav(leg, hex_pose)
-            found_pose = self.spin_search(leg)
+            self.gps_nav(hex_wp)
+            pose = self.spin_search()
             # Did the last spin search find it?
-            if found_pose:
-                return found_pose
+            if pose:
+                return pose
 
         return False
 
-    def aruco_check(self, leg):
+    def aruco_check(self):
         """
         Function to check for the aruco tag
         """
 
         if self.found_flag:
+            pose = self.found_pose
+            self.found_pose = None
             self.found_flag = False
-            return self.found_pose
+            return pose
         else:
             return False
 
-    def obj_check(self, leg):
+    def obj_check(self):
         """
         Function to check for the object
         """
@@ -381,17 +379,14 @@ class StateMachine(Node):
         Function to execute task legs
         """
 
-        self.get_logger().info("Executing leg: " + leg)
+        self.leg = leg
+        self.get_logger().info("Executing leg: " + self.leg)
 
         # Iterate through the GPS legs
-        if leg in self.gps_legs:
+        if self.leg in self.gps_legs:
 
-            self.get_logger().info(leg + " Starting GPS leg")
-            self.gps_path_nav(leg)
-
-            # Don't wait or flash the LED for the start leg
-            if leg == "start":
-                return
+            self.get_logger().info(self.leg + " Starting GPS leg")
+            self.gps_path_nav()
 
             # Trigger the arrival state
             future = self.arrival_client.call_async(self.arrival_request)
@@ -404,20 +399,20 @@ class StateMachine(Node):
             rclpy.spin_until_future_complete(self, future)
 
         # Iterate through the aruco legs
-        elif leg in self.aruco_legs:
+        elif self.leg in self.aruco_legs:
 
-            self.get_logger().info(leg + " Starting aruco leg")
-            self.gps_path_nav(leg)
+            self.get_logger().info(self.leg + " Starting aruco leg")
+            self.gps_path_nav()
 
             # Look for the aruco tag
-            aruco_loc = self.spin_search(leg)  # Do a spin search
+            aruco_loc = self.spin_search()  # Do a spin search
             if not aruco_loc:
-                aruco_loc = self.hex_search(leg)  # Do a hex search
+                aruco_loc = self.hex_search()  # Do a hex search
             if not aruco_loc:
-                self.get_logger().error(leg + " Could not find the aruco tag")
+                self.get_logger().error(self.leg + " Could not find the aruco tag")
             else:
-                self.get_logger().info(leg + " Found the aruco tag at:", aruco_loc)
-                self.pose_nav(leg, aruco_loc)
+                self.get_logger().info(self.leg + " Found the aruco tag at:", aruco_loc)
+                self.gps_nav(aruco_loc)
 
                 # Trigger the arrival state
                 future = self.arrival_client.call_async(self.arrival_request)
@@ -428,9 +423,6 @@ class StateMachine(Node):
                 # Trigger the autonomy state
                 future = self.nav_client.call_async(self.nav_request)
                 rclpy.spin_until_future_complete(self, future)
-
-            # Go back to the last GPS point
-            self.gps_point_nav(leg, self.wps[-1])
 
         # Iterate through the object legs
         elif leg in self.obj_legs:
@@ -458,9 +450,6 @@ class StateMachine(Node):
                 future = self.nav_client.call_async(self.nav_request)
                 rclpy.spin_until_future_complete(self, future)
 
-            # Go back to the last GPS point
-            self.gps_point_nav(leg, self.wps[-1])
-
     def run_state_machine(self):
         """
         Function to run the competition state machine
@@ -475,12 +464,10 @@ class StateMachine(Node):
                 break
             self.exec_leg(leg)
 
-        self.get_logger().info("State machine completed")
-
         # Trigger the teleop state
         future = self.teleop_client.call_async(self.teleop_request)
         rclpy.spin_until_future_complete(self, future)
-        self.get_logger().info("State machine node initialized")
+        self.get_logger().info("State machine completed")
 
 
 def spin_in_thread(exec):
---
+++ mars_ws/src/nav2_autonomy/nav2_autonomy/utils/plan_utils.py
@@ -0,0 +1,56 @@
+# Created by Nelson Durrant, Feb 2025
+import math
+from geographic_msgs.msg import GeoPose
+from nav2_autonomy.utils.gps_utils import latLonYawToGeopose
+
+
+def basicPathPlanner(self, geopose1, geopose2):
+        """
+        Generate intermediary waypoints in a straight line between two GPS coordinates
+
+        :author: Nelson Durrant
+        :date: Mar 2025
+        """
+
+        # Distance between intermediary waypoints (in lat/lon degrees)
+        # If the waypoints are too far apart, they won't be in the global costmap
+        # and the navigation2 stack won't be able to plan a path between them
+        STEP_SIZE = 0.0001
+
+        new_wps = []
+
+        # Get starting waypoint GPS coordinates
+        start_lat = geopose1.position.latitude
+        start_lon = geopose1.position.longitude
+        end_lat = geopose2.position.latitude
+        end_lon = geopose2.position.longitude
+
+        # Calculate the desire yaw angle for movement
+        yaw = math.atan((end_lat - start_lat) / (end_lon - start_lon))
+
+        # Calculate the distance between the two points
+        distance = ((end_lat - start_lat)**2 + (end_lon - start_lon)**2)**0.5
+
+        # Calculate the number of intermediary waypoints
+        num_waypoints = int(distance / STEP_SIZE)
+
+        if num_waypoints == 0:
+            return
+
+        # Calculate the step size for each intermediary waypoint
+        step_lat = (end_lat - start_lat) / num_waypoints
+        step_lon = (end_lon - start_lon) / num_waypoints
+
+        # Generate intermediary waypoints
+        for i in range(1, num_waypoints):
+            lat = start_lat + i * step_lat
+            lon = start_lon + i * step_lon
+            
+            geopose = latLonYawToGeopose(lat, lon, yaw)
+
+            new_wps.append(geopose)
+
+        # Add the original waypoint
+        new_wps.append(geopose2)
+
+        return new_wps
