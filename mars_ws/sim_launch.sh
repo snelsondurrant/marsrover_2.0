89570e519cb2eced70385f9c8ee8537298749a8b
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-04 16:48:28 -0700

    Updates

--- mars_ws/sim_launch.sh
+++ mars_ws/sim_launch.sh
@@ -3,9 +3,8 @@
 #
 # Launches the full simulation autonomy stack
 
-if [ ! "$(docker ps -q -f name=mapproxy)" ]; then
-    docker run -p 8080:8080 -d -t -v ~/mars_ws/src/nav2_autonomy/mapproxy:/mapproxy danielsnider/mapproxy
-fi
+# TODO: make sure the container isn't running
+docker run -p 8080:8080 -d -t -v ~/mars_ws/src/nav2_autonomy/mapproxy:/mapproxy danielsnider/mapproxy
 
 source ~/mars_ws/install/setup.bash
 ros2 launch nav2_autonomy rover_task_autonomy.launch.py use_rviz:=True use_mapviz:=True sim_mode:=True
