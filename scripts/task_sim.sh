2488cd2c37fb646edfcb9a108285803dba6d34a0
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-06 20:52:37 -0700

    Simulation docs

--- scripts/task_sim.sh
+++ scripts/task_sim.sh
@@ -4,10 +4,14 @@
 # Runs the autonomy task in the simulation environment
 
 echo ""
-echo -e "\033[0m\033[36mQUICK SIMULATION TIPS:\033[0m"
+echo "QUICK SIMULATION TIPS"
 echo "  - Left click on the turtlebot model ('World' tab, 'Models' dropdown) in Gazebo and click 'Follow' to keep the camera on the robot"
+echo "  - Click 'Fixed Frame' in the RViz toolbar and select 'base_link' to keep the visualizer centered on the robot"
 echo "  - Use the toolbar across the top of the Gazebo window to drag and drop obstacles into the robot's path"
 echo "  - Click 'Add' on the left toolbar in RViz, navigate to the 'By topic' tab, and select 'arduino_tracker/debug/Image' to see the camera feed with aruco tags labeled"
+echo "  - Click 'Add' on the left toolbar in RViz, select 'Detection3DArray' in the 'By display type' tab, and rename the topic in the dropdown to 'zed/detections' to see the object detection bounding boxes"
+echo ""
+echo "###"
 echo ""
 
 source ~/mars_ws/install/setup.bash
