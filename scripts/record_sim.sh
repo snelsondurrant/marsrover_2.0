3a20e02bb42b46cbefc0c2554a87fba323823609
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-08 16:01:57 -0700

    Record fix

--- scripts/record_sim.sh
+++ scripts/record_sim.sh
@@ -3,7 +3,9 @@
 # 
 # Records an autonomy task simulation run to a bag file
 
-source ~/mars_ws/install/setup.bash
-ros2 bag record -a -o ~/mars_ws/src/nav2_autonomy/bags/sim_run_$(date +%Y%m%d_%H%M%S)
+tag=sim_run_$(date +%Y%m%d_%H%M%S)
 
-ros2 run plotjuggler plotjuggler ~/mars_ws/src/nav2_autonomy/bags/sim_run_$(date +%Y%m%d_%H%M%S).db3
+source ~/mars_ws/install/setup.bash
+ros2 bag record -a -o ~/mars_ws/src/nav2_autonomy/bags/sim_run_$tag
+
+ros2 run plotjuggler plotjuggler ~/mars_ws/src/nav2_autonomy/bags/sim_run_$tag/sim_run_$tag.db3
