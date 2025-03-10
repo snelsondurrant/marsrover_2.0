d5e6d07873a5e597e9099d819cc941d27682f504
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-10 02:15:09 -0600

    Fixed process management

--- scripts/key_teleop.sh
+++ scripts/key_teleop.sh
@@ -5,6 +5,7 @@
 
 source ~/mars_ws/install/setup.bash
 ros2 run topic_tools relay /cmd_vel_teleop /cmd_vel & # remap to just /cmd_vel for simulation
+relay_pid=$! # save the pid of the relay process
 ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args --remap /cmd_vel:=/cmd_vel_teleop 
 
-kill %1 # Kill the topic relay process
+pkill -P $relay_pid # kill the relay process
