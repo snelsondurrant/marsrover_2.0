a124cb4ed29b6ca16d6f558a2c51dd8b6072fa01
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-11 15:26:13 -0600

    Removed ssh into container

--- docker/startup/entrypoint.sh
+++ docker/startup/entrypoint.sh
@@ -5,17 +5,10 @@
 # - This script won't throw any errors, but the container will crash immediately
 # - Be very careful editing it!
 
-# From ros_entrypoint.sh
-set -e
-source "/opt/ros/$ROS_DISTRO/setup.bash" # source ROS distro
-
 # Are we running on Jetson Orin architecture (the rover)?
 if [ "$(uname -m)" == "aarch64" ]; then
     tmuxp load -d /startup/workspaces/rover_startup.yaml
 fi
 
-# Start the SSH daemon in the Docker container
-sudo /usr/sbin/sshd -D
-
 # IMPORTANT! Keeps the container running
-exec /bin/bash
+exec $@
