5878eb5778f57707b6e21d44480db6f84b56244c
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-14 12:04:00 -0600

    Fix for plotjuggler

--- scripts/simulation/record_sim.sh
+++ scripts/simulation/record_sim.sh
@@ -8,4 +8,4 @@ tag=sim_run_$(date +%Y%m%d_%H%M%S)
 source ~/rover_ws/install/setup.bash
 ros2 bag record -a -o ~/rover_ws/src/rover_gazebo/bags/sim_run_$tag
 
-ros2 run plotjuggler plotjuggler ~/rover_ws/src/rover_gazebo/bags/sim_run_$tag/sim_run_$tag.db3
+ros2 run plotjuggler plotjuggler -d ~/rover_ws/src/rover_gazebo/bags/sim_run_$tag/metadata.yaml
