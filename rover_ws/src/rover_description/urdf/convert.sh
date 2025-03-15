65660010e015e2cee84575f93dd396f3486b9f80
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-14 22:04:42 -0600

    Quick fix

--- rover_ws/src/rover_description/urdf/convert.sh
+++ rover_ws/src/rover_description/urdf/convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Created by Nelson Durrant, Mar 2025
 # 
-# SImply converts our rover xacro file to a udrf file
+# Simply converts our rover xacro file to a udrf file
 
 source ~/rover_ws/install/setup.bash
 ros2 run xacro xacro rover.xacro > rover.urdf
