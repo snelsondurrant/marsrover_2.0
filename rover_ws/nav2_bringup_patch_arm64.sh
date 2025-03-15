e97e525f0c86da1594b0ef8da88576c230d18139
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-15 16:11:02 -0600

    Added nav2_bringup patch

---
+++ rover_ws/nav2_bringup_patch_arm64.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Created by Nelson Durrant, Feb 2025
+#
+# The 'nav2_bringup' package doesn't install on arm64 platforms, so we have to install it explicitly
+# https://github.com/ros-navigation/navigation2/issues/3766
+
+# check if arch is arm64
+if [ "$(uname -m)" = "aarch64" ]; then
+  # check if src/nav2_bringup does not exist
+  if [ ! -d "src/nav2_bringup" ]; then
+    curl -L https://api.github.com/repos/ros-planning/navigation2/tarball/1.2.2 \
+      | tar xz -C src/ --wildcards "*/nav2_bringup" --strip-components=1
+
+    # remove "turtlebot3_gazebo" dependency from nav2_bringup/package.xml since it has not arm64 build
+    sed -i '/turtlebot3_gazebo/d' src/nav2_bringup/package.xml
+  fi
+fi
