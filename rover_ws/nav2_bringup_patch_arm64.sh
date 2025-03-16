bf0c07f6ac0f9766eef6fbe5198f2c8fb26ca9e6
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-15 19:33:00 -0600

    update

--- rover_ws/nav2_bringup_patch_arm64.sh
+++ rover_ws/nav2_bringup_patch_arm64.sh
@@ -4,12 +4,6 @@
 # The 'nav2_bringup' package doesn't install on arm64 platforms, so we have to install it explicitly
 # https://github.com/ros-navigation/navigation2/issues/3766
 
-# Check if the nav2_bringup package is already installed
-if [ -d "src/nav2_bringup" ]; then
-  echo "nav2_bringup package already exists, skipping installation."
-  exit 0
-fi
-
 # check if arch is arm64
 if [ "$(uname -m)" = "aarch64" ]; then
   # check if src/nav2_bringup does not exist
