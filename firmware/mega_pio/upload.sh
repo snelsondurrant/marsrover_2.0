e9963e475ab45885eadb3017345dd2d561f60746
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-15 18:39:11 -0600

    Updates

--- firmware/mega_pio/upload.sh
+++ firmware/mega_pio/upload.sh
@@ -3,4 +3,11 @@
 #
 # Builds and uploads the latest code to the board
 
+# For some reason, I can only get this to work after a device reset
+symlink="/dev/rover/onBoardMega"
+device=$(readlink -f $symlink)
+sysfs_path=$(udevadm info --query=path --name=$device)
+bus_device=$(basename $(dirname $sysfs_path))
+sudo usbreset $bus_device
+
 pio run -t upload
