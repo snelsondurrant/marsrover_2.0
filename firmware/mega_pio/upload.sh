138153ce4d6d133e3905e93c0c1d3bc8bd170565
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-15 18:57:08 -0600

    testing

--- firmware/mega_pio/upload.sh
+++ firmware/mega_pio/upload.sh
@@ -4,13 +4,10 @@
 # Builds and uploads the latest code to the board
 
 # For some reason, I can only get this to work after a device reset
-symlink="/dev/rover/onBoardMega"
-# get bus and device number
-bus=$(udevadm info --query=property --name=$symlink | grep DEVBUS | cut -d'=' -f2)
+bus=$(lsusb | grep "Mega" | awk '{print $2}')
 echo "Bus: $bus"
-device=$(udevadm info --query=property --name=$symlink | grep DEVPID | cut -d'=' -f2)
+device=$(lsusb | grep "Mega" | awk '{print $4}')
 echo "Device: $device"
-# reset the device
 usbreset $bus/$device
 
 
