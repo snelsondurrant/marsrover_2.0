e0db176e98337cc4c3dd30eeb0f77320fe8c6096
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-15 18:48:45 -0600

    testing

--- firmware/mega_pio/upload.sh
+++ firmware/mega_pio/upload.sh
@@ -5,12 +5,13 @@
 
 # For some reason, I can only get this to work after a device reset
 symlink="/dev/rover/onBoardMega"
-device=$(readlink -f $symlink)
-echo "Resetting device $device"
-# Use lsusb to find bus and device numbers
-bus_device=$(lsusb | grep "$(basename $device)" | awk '{print $2"-"$4}' | sed 's/://g')
-echo "Bus-Device: $bus_device"
+# get the real path
+realpath=$(readlink -f $symlink)
+echo "Real path of the device: $realpath"
+# reset the device
+usbreset $realpath
+# wait for the device to reset
+sleep 2
 
-sudo usbreset $bus_device
 
 pio run -t upload
