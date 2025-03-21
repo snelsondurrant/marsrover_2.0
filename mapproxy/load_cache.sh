266abcc769630dca407112030f50248230d96486
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-21 02:25:28 -0600

    Mapproxy cache updates

--- mapproxy/load_cache.sh
+++ mapproxy/load_cache.sh
@@ -3,6 +3,15 @@
 # 
 # Preloads potential rover operation areas using MapProxy
 
-mapproxy-seed -f mapproxy.yaml -s seed.yaml -i
+function printError {
+  	# print red
+  	echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
+}
 
-# TODO: Set this up in Docker
+# Check to make sure this is being run with sudo
+if [ "$EUID" -ne 0 ]; then
+    printError "Please run as root (sudo bash load_cache.sh)"
+    exit
+fi
+
+mapproxy-seed -f mapproxy.yaml -s seed.yaml -i
