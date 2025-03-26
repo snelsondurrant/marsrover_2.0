e0d016be9f7f011de37a5234e10488c7fad1acdc
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-25 21:00:58 -0600

    Added zed calibration

---
+++ scripts/calibration/mag_calib.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Created by Nelson Durrant, Mar 2025
+#
+# Run this while the ZED is mounted on the rover to calibrate the ZED's magnetometer
+
+# WARNING! You need to connect using 'ssh -X' for GUI output if running this script remotely
+
+cd /usr/local/zed/tools
+./ZED_Sensor_Viewer
