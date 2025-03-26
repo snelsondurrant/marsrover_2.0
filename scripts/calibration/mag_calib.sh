325895d459ad038af345ba5d091214c168848264
Nelson Durrant <snelsondurrant@gmail.com>
2025-03-25 21:08:45 -0600

    ZED calibration updates

--- scripts/calibration/mag_calib.sh
+++ scripts/calibration/mag_calib.sh
@@ -2,6 +2,7 @@
 # Created by Nelson Durrant, Mar 2025
 #
 # Run this while the ZED is mounted on the rover to calibrate the ZED's magnetometer
+# https://www.stereolabs.com/docs/sensors/magnetometer#magnetometer-calibration
 
 # WARNING! You need to connect using 'ssh -X' for GUI output if running this script remotely
 
--- scripts/obj_det.sh
+++ scripts/obj_det.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Created by Nelson Durrant, Mar 2025
 # 
-# Force the rover to transition to the teleop state so we can control it
+# Enable object detection on the ZED camera
 
 source ~/rover_ws/install/setup.bash
 ros2 service call /zed/zed_node/enable_obj_det std_srvs/srv/SetBool "{data: true}"
