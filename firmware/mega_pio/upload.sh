88665c490b95a25293506e6cb07b56e444b71cd4
Nelson Durrant <snelsondurrant@gmail.com>
2025-04-07 20:44:15 -0600

    TODO updates

--- firmware/mega_pio/upload.sh
+++ firmware/mega_pio/upload.sh
@@ -3,9 +3,4 @@
 #
 # Builds and uploads the latest code to the board
 
-# TODO: For some reason, I can only get this to work after a device reset
-# bus=$(lsusb | grep "Mega" | awk '{print $2}')
-# device=$(lsusb | grep "Mega" | awk '{print $4}')
-# usbreset $bus/$device
-
 pio run -t upload
