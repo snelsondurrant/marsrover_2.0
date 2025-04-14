b73f1f17bb8ee6366f6cb79a402d7e0ce065c265
Nelson Durrant <snelsondurrant@gmail.com>
2025-04-14 11:00:18 -0600

    Fix for script

--- scripts/usb_reset.sh
+++ scripts/usb_reset.sh
@@ -6,8 +6,8 @@
 echo "Do you want to reset the Arduino Mega? (y/n)"
 read -r answer
 if [[ $answer == "y" || $answer == "Y" ]]; then
-    VENDOR=$(../udev/info.sh /dev/rover/onBoardMega | grep idVendor | head -n 1 | sed 's/.*="//; s/"$//')
-    PRODUCT=$(../udev/info.sh /dev/rover/onBoardMega | grep idProduct | head -n 1 | sed 's/.*="//; s/"$//')
+    VENDOR=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/onBoardMega) | grep idVendor | head -n 1 | sed 's/.*="//; s/"$//')
+    PRODUCT=$(udevadm info --attribute-walk --path $(udevadm info --query path --name $1/dev/rover/onBoardMega) | grep idProduct | head -n 1 | sed 's/.*="//; s/"$//')
     sudo usbreset $VENDOR:$PRODUCT
     echo "Arduino Mega reset"
 fi
@@ -15,8 +15,8 @@ fi
 echo "Do you want to reset the RTK GPS? (y/n)"
 read -r answer
 if [[ $answer == "y" || $answer == "Y" ]]; then
-    VENDOR=$(../udev/info.sh /dev/rover/rtk | grep idVendor | head -n 1 | sed 's/.*="//; s/"$//')
-    PRODUCT=$(../udev/info.sh /dev/rover/rtk | grep idProduct | head -n 1 | sed 's/.*="//; s/"$//')
+    VENDOR=(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/rtk) | grep idVendor | head -n 1 | sed 's/.*="//; s/"$//')
+    PRODUCT=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/rtk) | grep idProduct | head -n 1 | sed 's/.*="//; s/"$//')
     sudo usbreset $VENDOR:$PRODUCT
     echo "RTK GPS reset"
 fi
