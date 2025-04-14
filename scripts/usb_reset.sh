b9eb7acfdc06d9966dbde9d6d7e5c972ad09c639
Nelson Durrant <snelsondurrant@gmail.com>
2025-04-14 10:38:00 -0600

    Added reset script

---
+++ scripts/usb_reset.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Created by Nelson Durrant, Apr 2025
+# 
+# Interactively resets the USB devices on the rover
+
+echo "Do you want to reset the Arduino Mega? (y/n)"
+read -r answer
+if [[ $answer == "y" || $answer == "Y" ]]; then
+    VENDOR=$(../udev/info.sh /dev/rover/onBoardMega | grep idVendor | head -n 1 | sed 's/.*="//; s/"$//')
+    PRODUCT=$(../udev/info.sh /dev/rover/onBoardMega | grep idProduct | head -n 1 | sed 's/.*="//; s/"$//')
+    sudo usbreset $VENDOR:$PRODUCT
+    echo "Arduino Mega reset"
+fi
+
+echo "Do you want to reset the RTK GPS? (y/n)"
+read -r answer
+if [[ $answer == "y" || $answer == "Y" ]]; then
+    VENDOR=$(../udev/info.sh /dev/rover/rtk | grep idVendor | head -n 1 | sed 's/.*="//; s/"$//')
+    PRODUCT=$(../udev/info.sh /dev/rover/rtk | grep idProduct | head -n 1 | sed 's/.*="//; s/"$//')
+    sudo usbreset $VENDOR:$PRODUCT
+    echo "RTK GPS reset"
+fi
