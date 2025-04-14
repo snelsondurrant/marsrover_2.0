51340b3dea38f0c536bdf97fd20acdbb0e4943db
Nelson Durrant <snelsondurrant@gmail.com>
2025-04-14 11:16:26 -0600

    Quick fix

--- docker/usb_reset.sh
+++ docker/usb_reset.sh
@@ -3,7 +3,7 @@
 # 
 # Interactively resets the USB devices on the rover
 
-read -p "Do you want to reset the Arduino Mega? (y/n)" answer
+read -p "Do you want to reset the Arduino Mega? (y/n): " answer
 if [[ $answer == "y" || $answer == "Y" ]]; then
     VENDOR=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/onBoardMega) | grep idVendor | head -n 1 | sed 's/.*="//; s/"$//')
     PRODUCT=$(udevadm info --attribute-walk --path $(udevadm info --query path --name $1/dev/rover/onBoardMega) | grep idProduct | head -n 1 | sed 's/.*="//; s/"$//')
@@ -11,7 +11,7 @@ if [[ $answer == "y" || $answer == "Y" ]]; then
     echo "Arduino Mega reset"
 fi
 
-read -p "Do you want to reset the Arduino Nano? (y/n)" answer
+read -p "Do you want to reset the Arduino Nano? (y/n): " answer
 if [[ $answer == "y" || $answer == "Y" ]]; then
     VENDOR=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/peripheralsBoard) | grep idVendor | head -n 1 | sed 's/.*="//; s/"$//')
     PRODUCT=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/peripheralsBoard) | grep idProduct | head -n 1 | sed 's/.*="//; s/"$//')
@@ -19,7 +19,7 @@ if [[ $answer == "y" || $answer == "Y" ]]; then
     echo "Arduino Nano reset"
 fi
 
-read -p "Do you want to reset the RTK GPS? (y/n)" answer
+read -p "Do you want to reset the RTK GPS? (y/n): " answer
 if [[ $answer == "y" || $answer == "Y" ]]; then
     VENDOR=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/rtk) | grep idVendor | head -n 1 | sed 's/.*="//; s/"$//')
     PRODUCT=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/rtk) | grep idProduct | head -n 1 | sed 's/.*="//; s/"$//')
@@ -27,7 +27,7 @@ if [[ $answer == "y" || $answer == "Y" ]]; then
     echo "RTK GPS reset"
 fi
 
-read -p "Do you want to reset the ZED camera? (y/n)" answer
+read -p "Do you want to reset the ZED camera? (y/n): " answer
 if [[ $answer == "y" || $answer == "Y" ]]; then
     VENDOR=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/cameras/ZED_front) | grep idVendor | head -n 1 | sed 's/.*="//; s/"$//')
     PRODUCT=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/cameras/ZED_front) | grep idProduct | head -n 1 | sed 's/.*="//; s/"$//')
