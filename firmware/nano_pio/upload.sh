2ac35ca5e99c91fd05817ff7027a65c6bb3c7935
Nelson Durrant <snelsondurrant@gmail.com>
2025-04-16 01:00:34 -0600

    Updates

--- firmware/nano_pio/upload.sh
+++ firmware/nano_pio/upload.sh
@@ -3,10 +3,9 @@
 #
 # Builds and uploads the latest Arduino Nano code to the board
 
-VENDOR=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/peripheralsBoard) | grep idVendor | head -n 1 | sed 's/.*="//; s/"$//')
-PRODUCT=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/peripheralsBoard) | grep idProduct | head -n 1 | sed 's/.*="//; s/"$//')
-sudo usbreset $VENDOR:$PRODUCT
+# VENDOR=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/peripheralsBoard) | grep idVendor | head -n 1 | sed 's/.*="//; s/"$//')
+# PRODUCT=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/peripheralsBoard) | grep idProduct | head -n 1 | sed 's/.*="//; s/"$//')
+# sudo usbreset $VENDOR:$PRODUCT
+# sleep 1
 
-sleep 1
-
-pio run -t upload --upload-port /dev/rover/peripheralsBoard
+pio run -t upload # --upload-port /dev/rover/peripheralsBoard
