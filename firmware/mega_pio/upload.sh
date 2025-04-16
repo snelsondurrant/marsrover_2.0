175da7da18dbcd5c90bf0bf6fc557996e744f909
Nelson Durrant <snelsondurrant@gmail.com>
2025-04-16 00:47:59 -0600

    Updates

--- firmware/mega_pio/upload.sh
+++ firmware/mega_pio/upload.sh
@@ -6,4 +6,8 @@
 # TODO: For some reason this only works on the rover immediately after plugging in the mega directly,
 # and then only sometimes -- it could use some research and debugging.
 
-pio run -t upload
+VENDOR=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/onBoardMega) | grep idVendor | head -n 1 | sed 's/.*="//; s/"$//')
+PRODUCT=$(udevadm info --attribute-walk --path $(udevadm info --query path --name $1/dev/rover/onBoardMega) | grep idProduct | head -n 1 | sed 's/.*="//; s/"$//')
+sudo usbreset $VENDOR:$PRODUCT
+
+pio run -t upload --upload-port /dev/rover/onBoardMega
--- firmware/nano_pio/upload.sh
+++ firmware/nano_pio/upload.sh
@@ -3,4 +3,8 @@
 #
 # Builds and uploads the latest Arduino Nano code to the board
 
-pio run -t upload
+VENDOR=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/peripheralsBoard) | grep idVendor | head -n 1 | sed 's/.*="//; s/"$//')
+PRODUCT=$(udevadm info --attribute-walk --path $(udevadm info --query path --name /dev/rover/peripheralsBoard) | grep idProduct | head -n 1 | sed 's/.*="//; s/"$//')
+sudo usbreset $VENDOR:$PRODUCT
+
+pio run -t upload --upload-port /dev/rover/peripheralsBoard
