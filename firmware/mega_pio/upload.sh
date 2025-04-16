0aca70e5d3f0a0733dbb91b1ef5233df76270cb8
Nelson Durrant <snelsondurrant@gmail.com>
2025-04-16 00:33:44 -0600

    Updates

--- firmware/mega_pio/upload.sh
+++ firmware/mega_pio/upload.sh
@@ -3,7 +3,7 @@
 #
 # Builds and uploads the latest Arduino Mega code to the board
 
-# TODO: For some reason this only works immediately after plugging the mega into the rover.
+# TODO: For some reason this only works immediately after plugging the mega directly into the rover.
 # It could use some research and debugging.
 
 pio run -t upload
