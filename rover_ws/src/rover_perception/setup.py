a15ad1209c0d166769678d2f8258119508c0fa5f
Nelson Durrant <snelsondurrant@gmail.com>
2025-07-12 23:41:38 -0500

    Added optimized Dockerfile

--- rover_ws/src/rover_perception/setup.py
+++ rover_ws/src/rover_perception/setup.py
@@ -6,7 +6,7 @@ package_name = 'rover_perception'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
