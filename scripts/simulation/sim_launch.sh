ed44a1b5d161a8a0e4ec01a3de3c6ae8b813d2df
Nelson Durrant <snelsondurrant@gmail.com>
2025-07-17 13:26:38 -0500

    Exploring remapping options

--- scripts/simulation/sim_launch.sh
+++ scripts/simulation/sim_launch.sh
@@ -4,4 +4,4 @@
 # Launches the full autonomy task simulation
 
 source ~/rover_ws/install/setup.bash
-ros2 launch rover_bringup rover_autonomy.launch.py sim_mode:=True use_rviz:=True use_mapviz:=True
+ros2 launch rover_bringup rover_autonomy.launch.py sim_mode:=True use_rviz:=True use_mapviz:=False
